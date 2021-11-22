1d8271b917c91bb7d453e070c070e93c50b7c548
Hallam Rear <hallamrear@gmail.com>
2021-11-22 03:17:40 +0000

    Renamed Project.

---
+++ CellularAutomata/Simulation/entry.cpp
@@ -0,0 +1,44 @@
+#include "GameOfLife.h"
+#include "LangtonsAnt.h"
+#include "HalTec\Game.h"
+#include "HalTec\StateDirector.h"
+#include "HalTec\InputManager.h"
+#include "HalTec/TextElement.h"
+
+class Menu : public GameState
+{
+	Transform transform;
+	TextElement* mText;
+
+	inline void Start()
+	{
+		mText = new TextElement(transform, "");
+
+		InputManager::Bind(IM_KEY_CODE::IM_KEY_F1, IM_KEY_STATE::IM_KEY_PRESSED, [this]() { StateDirector::SetState(GameStateIdentifier::GAME_STATE_1); });
+		InputManager::Bind(IM_KEY_CODE::IM_KEY_F2, IM_KEY_STATE::IM_KEY_PRESSED, [this]() { StateDirector::SetState(GameStateIdentifier::GAME_STATE_2); });
+	};
+
+	inline void End() { delete mText; mText = nullptr; };
+	inline void Update(double DeltaTime) { mText->SetString("Press F1 for Conway's Game of Life. Press F2 for Langton's ant."); mText->Update(DeltaTime); };
+	inline void Render(SDL_Renderer& renderer) { mText->Render(); };
+};
+
+int main(int argc, char* argv[])
+{
+	StateDirector::SetupState(GameStateIdentifier::GAME_STATE_MAIN_MENU, new Menu());
+	StateDirector::SetupState(GameStateIdentifier::GAME_STATE_1, new GameOfLife());
+	StateDirector::SetupState(GameStateIdentifier::GAME_STATE_2, new LangtonsAnt());
+	StateDirector::SetState(GameStateIdentifier::GAME_STATE_MAIN_MENU);
+
+	WindowDetails details;
+	Game game;
+
+	game.Initialise(argc, argv, details);
+
+	if(game.GetIsInitialised())
+	{
+		game.Start();
+	}
+
+	return 0;
+}
